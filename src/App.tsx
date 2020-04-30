f5b8c6f64be88da9e72e4cc692d1c4ab7f00f708
jaydenkwright <jaydenkwright3@gmail.com>
2020-04-30 18:00:53 -0400

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
 import './App.css';
+import styles from './App.module.css'
+import Stock from './components/Stock'
 
 function App() {
+  const [stockData, setStockData] = useState([])
+  const [query, setQuery] = useState<string | null>('')
+  const [isSearch, setIsSearching] = useState<boolean>(false)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stock />
     </div>
   );
 }
