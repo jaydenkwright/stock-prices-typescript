2c3865c12455e981d07922f864b6dba76e0cfe1a
jaydenkwright <jaydenkwright3@gmail.com>
2020-05-03 18:37:52 -0400

    News data is retreived from News API and saved into state with useState() hook

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './News.module.css'
+import axios from 'axios'
 
 export const News: React.FC =() => {
-    const getNewsData = () => {
+
+    interface article{
+        author: string,
+        content: string,
+        description: string,
+        publishedAt: string,
+        source: {
+            id: any,
+            name: string
+        },
+        title: string,
+        url: string,
+        urlToImage: string
     }
+
+    const [newsData, setNewsData] = useState<article[]>([])
+
+    const getNewsData = async (): Promise<void> => {
+        try{
+            const res = await axios.get(`https://newsapi.org/v2/everything?q=google&apiKey=${process.env.REACT_APP_NEWS_KEY}`)
+            console.log(res.data.articles)
+            setNewsData(res.data.articles)
+            console.log(newsData)
+            //res.data.articles.map((article: any) => console.log(article.title))
+        }catch(err){
+            
+        }
+    }
+
+    useEffect(() => {
+        getNewsData()
+    }, [])
     return (
         <div className={styles.news}>
             <div className={styles.newsTitle}>
                 News
+                {
+                    newsData.map((a: any) => <div>{a.title}</div>)
+                }
             </div>
         </div>
     )
